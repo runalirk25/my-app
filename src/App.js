4ef72e868c7d3393268c655cd258a10f5cff51b2
runalirk25 <runali.k@sankeysolutions.com>
2025-06-19 18:28:40 +0530

    updated yaml file 7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Finally able to DEPLOY.
         </p>
         <a
           className="App-link"
